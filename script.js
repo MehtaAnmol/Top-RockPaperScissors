1e579300b13d16917150570a69711f630522a4de
AnmolMehta <anmolmehta1213@gmail.com>
2024-04-26 15:50:02 +0530

    Added css and  newGame button

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const rockButton = document.querySelector(".rock-button-js");
 const paperButton = document.querySelector(".paper-button-js");
 const scissorButton = document.querySelector(".scissors-button-js");
+const Winner = document.querySelector(".result-display-js");
+const score = document.querySelector(".scores");
+const startNewGame = document.querySelector(".new-game-button");
 
 
 function computersChoice(){
@@ -20,21 +23,32 @@ paperButton.addEventListener("click", () =>{
 scissorButton.addEventListener("click", () =>{
     startGame('scissors');
 });
-let count = 1;
+let count = 0;
 function startGame(playerChoice){
     let computer = computersChoice();
     let player = playerChoice;
     let result= checkWinner(computer , player);
+    displayChoices(player , computer);
     if(count < 6){
         count++;
         displayWinner(result, player, computer);
-        
-        console.log(count);
     }else{
-        startNewGame();
+        resetScores()
     }
 }
 
+function displayChoices(player, computer){
+    let playerDisplay = ""
+    let computerDisplay = ""
+    playerDisplay += `
+    <img src="images/${player}-emoji.png" class = "move-player"></img>
+    `;
+    computerDisplay +=`
+    <img src="images/${computer}-emoji.png" class = "move-computer"></img>
+    `
+    document.querySelector(".player").innerHTML = playerDisplay;
+    document.querySelector(".computer").innerHTML = computerDisplay;
+}
 function checkWinner(computer, player){
     if(player === 'rock'){
         if(computer === 'rock') return 'draw';
@@ -50,28 +64,39 @@ function checkWinner(computer, player){
         if(computer === 'scissors') return 'draw';
     }
 }
-
+let playerWon = 0;
+let computerWon = 0;
+let draw = 0;
 function displayWinner(result, player, computer){
-    // console.log(result)
-    // console.log(`player : ${player}  computer : ${computer}`)
-    let playerDisplay = "";
-    let computerDisplay = "";
-    let resultDisplay = "";
-    playerDisplay += player;
-    computerDisplay += computer;
     
+    Winner.innerHTML = "";
+    let resultDisplay = "";
     if(result === 'won'){
+        playerWon +=1 ;
         resultDisplay += 'Player wins';
     }else if(result === 'lost'){
+        computerWon += 1;
         resultDisplay += 'Computer wins';
     }else{
+        draw += 1;
         resultDisplay += 'Draw'
     }
-     
-    document.querySelector(".player-choice-js").innerHTML = playerDisplay;
-    document.querySelector(".computer-choice-js").innerHTML = computerDisplay;
-    document.querySelector(".result-display-js").innerHTML = resultDisplay;
+    Winner.innerHTML = resultDisplay;
+    score.innerHTML = `
+        Player: ${playerWon}  Computer: ${computerWon}  draw: ${draw}; 
+    `
 }
-function startNewGame(){
+function resetScores(){
     count = 0;
+    playerWon = 0;
+    computerWon = 0;
+    draw = 0;
+    let resultDisplay = "";
+    score.innerHTML = `
+        Player: ${playerWon}  Computer: ${computerWon}  draw: ${draw}; 
+    `
 }
+
+startNewGame.addEventListener("click" , () => {
+    resetScores();
+})
